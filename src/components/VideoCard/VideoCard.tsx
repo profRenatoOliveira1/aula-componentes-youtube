fb91abac586456bc66c98dba65e2097bdc12ef84
Renato Oliveira <renato.loliveira89@gmail.com>
2025-03-19 10:35:37 -0300

    Criando componentes dinâmicos - aula youtube

--- src/components/VideoCard/VideoCard.tsx
+++ src/components/VideoCard/VideoCard.tsx
@@ -1,21 +1,28 @@
 import estilo from './VideoCard.module.css';
+import respostaServidor from '../../../server/videolist.json';
 
 function VideoCard() {
     return (
-        <div className={estilo.videoCard}>
-            <div className={estilo.videoThumb}>
-                <img src="https://cdn.fliki.ai/image/page/660ba680adaa44a37532fd97/6663112070e1cfda27f86585.jpg" alt="thumbnail" />
-            </div>
-            <div className={estilo.videoInfo}>
-                <div className={estilo.channelLogo}>
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSazpOCLQHTS0gciqWm2sT7HZrUCHP9kKemdQ&s" alt="capa canal" />
-                </div>
-                <div className={estilo.videoText}>
-                    <p className={estilo.videoTitle}>Título do vídeo</p>
-                    <p>Canal</p>
-                    <p>13 mi visualizações</p>
-                </div>
-            </div>
+        <div className={estilo.videoCardContainer}>
+            {
+                respostaServidor.map((video, index) => (
+                    <div className={estilo.videoCard} key={index}>
+                        <div className={estilo.videoThumb}>
+                            <img src={video.thumb} alt="thumbnail" />
+                        </div>
+                        <div className={estilo.videoInfo}>
+                            <div className={estilo.channelLogo}>
+                                <img src={video['channel-logo']} alt="capa canal" />
+                            </div>
+                            <div className={estilo.videoText}>
+                                <p className={estilo.videoTitle}>{video.title}</p>
+                                <p>{video['channel-name']}</p>
+                                <p>{video.views}</p>
+                            </div>
+                        </div>
+                    </div>
+                ))
+            }
         </div>
     );
 }
